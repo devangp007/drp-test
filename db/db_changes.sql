3f8993ae6032fde687c13fc47f8fc85dd65b6f77
devangp007 <devangp007@yahoo.com>
2016-08-15 21:24:34 -0400

    added mname column to db_changes

--- db/db_changes.sql
+++ db/db_changes.sql
@@ -1,3 +1,4 @@
+/*
 create table drpt2
 (
 id int identity(1,1),
@@ -5,4 +6,7 @@ fname varchar(32),
 lname varchar(32),
 create_dt datetime default(getdate())
 )
+go
+*/
+alter table drpt2 add mname varchar(32)
 go
