981540ab944d4c3ab536541ba041edec1c32ce55
devangp007 <devangp007@yahoo.com>
2016-08-15 21:38:53 -0400

    recreated drpt2 table with mname in the middle

--- db/db_changes.sql
+++ db/db_changes.sql
@@ -7,6 +7,20 @@ lname varchar(32),
 create_dt datetime default(getdate())
 )
 go
-*/
+
 alter table drpt2 add mname varchar(32)
+go
+*/
+if (OBJECT_ID('adventureworks.dbo.drpt2') is not null)
+drop table dbo.drpt2
+go
+
+create table drpt2
+(
+id int identity(1,1),
+fname varchar(32),
+mname varchar(32),
+lname varchar(32),
+create_dt datetime default(getdate())
+)
 go
